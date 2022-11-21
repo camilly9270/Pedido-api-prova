901e573348b40f05752ae005fadd9f82f76b1152
camilly9270 <camilly.ccn@gmail.com>
2022-11-21 19:04:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pedido-api-prova
---
+++ src/test/java/com/example/PedidoApi/PedidoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PedidoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
